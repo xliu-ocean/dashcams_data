ab1119cc99ead3899bb162e2876042a8e18c289e
Xiaohui Liu <xliu@10-248-64-33.wireless.oregonstate.edu>
2023-12-13 13:13:24 -0800

    revised cross covariance

--- xcovar.m
+++ xcovar.m
@@ -6,7 +6,9 @@ function [Rxy,mux,s2x,muy,s2y,k,Nk] = xcovar(x,y,kkk)
  s2x = sum((x-mux).^2,'omitnan');
  s2y = sum((y-muy).^2,'omitnan');
 
- Rxy = sum((x-mux).*(y-muy),'omitnan');
+ for ik = 1:100
+     Rxy(ik) = sum((x(1:end-ik+1)-mux).*(y(ik:end)-muy),'omitnan');
+ end
 
  k=1:kkk;
  Nk=kkk;
